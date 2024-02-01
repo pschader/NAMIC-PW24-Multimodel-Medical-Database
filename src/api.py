380aadcb363d15863534a0fc41d9f763735d8d85
Maria Monzon <mmonzon@ethz.ch>
2024-02-01 17:08:55 +0000

    add basic example for retrieveng dicom studyInstance UID

--- src/api.py
+++ src/api.py
@@ -6,15 +6,18 @@ from pathlib import Path
 from pynetdicom import AE, StoragePresentationContexts
 from requests.auth import HTTPBasicAuth
 
-from src.metadata import session
+from src.dicom import DICOM_TAGS
 from src.utils import get_environment_variable
 
-# I
+ORTHANC_SERVER = "http://localhost:8042"
+ORTHANC_USER = get_environment_variable("ORTHANC_USER", "admin")
+ORTHANC_PW = get_environment_variable("ORTHANC_PW", "admin")
+
 session = requests.Session()
 
 def send_dicom_to_pacs(series_path, pacs_host, pacs_port):
     # Initialize the Application Entity
-    ae = AE(ae_title='PYNETDICOM', port=2000)
+    ae = AE(ae_title='ORTHANC')
 
     # Add a requested presentation context
     ae.requested_contexts = StoragePresentationContexts
@@ -43,73 +46,6 @@ def send_dicom_to_pacs(series_path, pacs_host, pacs_port):
 
         # Release the association
         assoc.release()
-
-if __name__=='__main__':
-
-    # Define the host and port of the PACS
-    pacs_host = 'localhosts'
-    pacs_port = 4242
-
-    # Define the path to the DICOM file
-    file_path = '/Users/mmonzon/Downloads/TCIA/manifest-1603198545583/NSCLC-Radiomics/LUNG1-006/1.3.6.1.4.1.32722.99.99.270361505197008655909592732352678399263'
-
-    # Send the DICOM file to the PACS
-    send_dicom_to_pacs(file_path, pacs_host, pacs_port)
-ORTHANC_SERVER = "http://localhost:8042"
-DICOM_TAGS = {
-    "PatientID": "00100020",
-    "PatientName": "00100010",
-    "PatientBirthDate": "00100030",
-    "PatientSex": "00100040",
-    # Study level tags
-    "StudyInstanceUID": "0020000D",
-    "StudyDate": "00080020",
-    "StudyTime": "00080030",
-    "StudyDescription": "00081030",
-    "StudyID": "00200010",
-    "AccessionNumber": "00080050",
-    "Modality": "00080060",
-    "Manufacturer": "00080070",
-    "InstitutionName": "00080080",
-
-    # Series level tags
-    "SeriesInstanceUID": "0020000E",
-    "SeriesDescription": "0008103E",
-    "SeriesNumber": "00200011",
-    "BodyPartExamined": "00180015",
-    "ProtocolName": "00181030",
-    "PatientPosition": "00185100",
-    "PixelSpacing": "00280030",
-    "Rows": "00280010",
-    "Columns": "00280011",
-    "PixelRepresentation": "00280103",
-    "WindowCenter": "00281050",
-    "WindowWidth": "00281051",
-    "RescaleIntercept": "00281052",
-    "RescaleSlope": "00281053",
-
-    # Instance level tags
-    "InstanceNumber": "00200013",
-    "SOPInstanceUID": "00080018",
-    "SOPClassUID": "00080016",
-    "InstanceCreationDate": "00080012",
-    "InstanceCreationTime": "00080013",
-    "ImageType": "00080008",
-    # Other tags
-    "AcquisitionNumber": "00200012",
-    "AcquisitionDate": "00080022",
-    "AcquisitionTime": "00080032",
-    "AcquisitionDateTime": "0008002A",
-    "ContentDate": "00080023",
-    "ContentTime": "00080033",
-    "ContentDateTime": "00080023",
-    "SeriesDate": "00080021",
-    "SeriesTime": "00080031",
-    "PerformedProcedureStepStartDate": "00400244",
-    "PerformedProcedureStepStartTime": "00400245",
-}
-ORTHANC_USER = get_environment_variable("ORTHANC_USER", "admin")
-ORTHANC_PW = get_environment_variable("ORTHANC_PW", "admin")
 
 
 def retrieve_data(endpoint_url, auth = None):
@@ -158,13 +94,25 @@ def get_patients_ids(qido_rs_endpoint, auth = None):
 
     # Extract and print the Patient IDs
     if response.status_code == 200:
-        patients = response.json()
-        patient_ids = [patient['00100020']['Value'][0] for patient in patients]
+        metadata = response.json()
+        patient_ids = [m.get(DICOM_TAGS["PatientID"])['Value'][0] for m in metadata]
         return patient_ids
 
     else:
         print(f"Error: {response.status_code}")
         return None
+
+def get_studies_ids(qido_rs_endpoint, auth = None):
+
+    # Send a GET request to the QIDO-RS endpoint
+    response = requests.get(qido_rs_endpoint, auth = auth)
+
+    # Extract and print the Patient IDs
+    if response.status_code == 200:
+        json_response = response.json()
+        studies_ids = [dicom_metadata[DICOM_TAGS['StudyInstanceUID']]['Value'][0] for dicom_metadata in json_response]
+        return studies_ids
+
 
 
 def test_orthanc():
@@ -179,12 +127,34 @@ def test_orthanc():
     # Define the QIDO-RS endpoint for querying all patients
     qido_rs_endpoint = "/dicom-web/studies"
     wado_rs_endpoint = "/dicom-web/wado"
+
     # Send a GET request to the QIDO-RS endpoint
     response = requests.get(ORTHANC_SERVER + qido_rs_endpoint, auth=session.auth)
     # Parse the JSON response
     json_response = response.json()
+
     # Extract the patient IDs from the response
     patient_ids = [dicom_metadata[DICOM_TAGS['PatientID']]['Value'][0] for dicom_metadata in json_response]
-    patient_ids = [dicom_metadata.get(DICOM_TAGS['StudyID']) for dicom_metadata in json_response]
+    studies_ids = [dicom_metadata[DICOM_TAGS['StudyInstanceUID']]['Value'][0] for dicom_metadata in json_response]
+
+
+    metadata = requests.get(ORTHANC_SERVER + qido_rs_endpoint, auth=session.auth)
+
     # Extract and print the Patient IDs
     print("Patient IDs:", patient_ids)
+
+
+if __name__=='__main__':
+
+    # Define the host and port of the PACS
+    pacs_host = 'localhosts'
+    pacs_port = 4242
+
+    # Define the path to the DICOM file
+    file_path = './data/TCIA/manifest-1603198545583/NSCLC-Radiomics/LUNG1-006/1.3.6.1.4.1.32722.99.99.270361505197008655909592732352678399263'
+
+    test_orthanc()
+    # Send the DICOM file to the PACS
+    #send_dicom_to_pacs(file_path, pacs_host, pacs_port)
+
+
