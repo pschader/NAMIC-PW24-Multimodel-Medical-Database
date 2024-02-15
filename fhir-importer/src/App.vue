3c13e9715967e08c2035028f2174ad49c360a6a7
Odile Elias <odileelias@e230-mb008-wl.local>
2024-02-15 09:54:28 +0100

    Changes in namic fhir-wise

--- fhir-importer/src/App.vue
+++ fhir-importer/src/App.vue
@@ -3,5 +3,25 @@
 </template>
 
 <script lang="ts" setup>
-  //
+import { onMounted } from 'vue';
+//import FHIR from 'your-fhir-library'; // Import your FHIR library
+import FHIR from "fhirclient";
+onMounted(async () => {
+  const client = FHIR.client("http://localhost:8080/fhir");
+  const bundle = await client.request("Patient");
+  console.log(bundle);
+ /*const patientData = {
+        "resourceType": "Patient",
+        "id": "testId",
+        "name": [{
+          "family": "Doe",
+          "given": ["John"]
+        }],
+        "gender": "male",
+        "birthDate": "1960-0143-03"
+      }
+      const client = FHIR.client("http://localhost:8080/fhir");
+      const createBundle = await client.create(patientData);
+      console.log(createBundle);*/
+});
 </script>
--- fhir-importer/src/components/CsvDrop.vue
+++ fhir-importer/src/components/CsvDrop.vue
@@ -3,7 +3,7 @@
     <v-row>
       <v-row>
         <v-col cols="12">
-          <v-text-field label="Coding System" variant="solo"></v-text-field>
+          <v-text-field label="Coding System" variant="solo" v-model="codingSystem"></v-text-field>
         </v-col>
       </v-row>
       <v-col cols="12">
@@ -23,17 +23,23 @@
           <thead>
             <tr>
               <td>CSV Row</td>
-              <td>FHIR Ressource</td>
-              <td>FHIR Field</td>
-              <td>Action</td>
+              <td>FHIR Resource</td>
+              <td>Resource parameter</td>
+              <td>Data type</td>
+              <td>Unit</td>
             </tr>
           </thead>
           <tbody>
             <tr v-for="header in headers" :key="header.key">
               <td>{{ header.key }}</td>
-              <td><v-combobox label="Ressource" v-model="mapping[header.key]['ressource']" :items="['Ignore', 'Observation', 'Patient']" variant="outlined" ></v-combobox></td>
-              <td><v-combobox label="Field" v-model="mapping[header.key]['field']" :items="['Value', 'Subject']" variant="outlined" ></v-combobox></td>
-              <td><v-combobox label="Action" v-model="mapping[header.key]['action']" :items="['Cast to Int', 'Subject']" variant="outlined" ></v-combobox></td>
+              <td><v-combobox label="Resource" v-model="mapping[header.key]['resource']" :items="resourceItems"
+                  variant="outlined"></v-combobox></td>
+              <td><v-combobox label="parameter" v-model="mapping[header.key]['parameter']" :items="patientItems"
+                  variant="outlined"></v-combobox></td>
+              <td><v-combobox label="datatype" v-model="mapping[header.key]['datatype']" :items="datatypeItems"
+                  variant="outlined"></v-combobox></td>
+              <td><v-text-field label="Unit" v-model="mapping[header.key]['unit']" variant="outlined">
+                </v-text-field></td>
             </tr>
           </tbody>
         </v-table>
@@ -41,7 +47,7 @@
     </v-row>
     <v-row v-if="headers.length !== 0">
       <v-col cols="12">
-        <v-textarea label="FHIR Ressources" variant="solo"></v-textarea>
+        <v-textarea label="FHIR Resources" variant="solo"></v-textarea>
       </v-col>
     </v-row>
     <v-row v-if="headers.length !== 0">
@@ -54,10 +60,17 @@
 
 <script>
 import { useCsvStore } from '@/store/csv.ts'
+import FHIR from "fhirclient";
+
 export default {
+
   data() {
     return {
+      codingSystem: "",
       items: [],
+      resourceItems: ['Ignore', 'Observation', 'Patient'],
+      patientItems: ['identifier', 'gender', 'deceased'],
+      datatypeItems: ['Integer', 'Boolean', 'String'],
       headers: [],
       mapping: {},
     };
@@ -66,6 +79,10 @@ export default {
     handleDrop(e) {
       const file = e.dataTransfer.files[0];
       this.parseCsv(file);
+    },
+    async createFhirResource(fhirData, resourceName) {
+      const client = FHIR.client("http://localhost:8080/fhir/");
+      const createFhirResource = await client.create(fhirData);
     },
     parseCsv(file) {
       const reader = new FileReader();
@@ -86,13 +103,109 @@ export default {
       };
       reader.readAsText(file);
     },
-    onChange(e) {
-      // for(var key in this.mapping) {
-      //   const ressource = this.mapping[key]['ressource']
-      //   const field = this.mapping[key]['field']
-      //   console.log();
-      // }
+    onMounted() {
+      this.createFhirResource
+    },
+    async onChange(e) {
+      for (var item in this.items) {
+        var theItem = this.items[item];
+        var identifierValue = null
+        var identifierCode = null
+        var genderValue = null
+        var deceasedValue = null
+        var valueString = null
+        var valueQuantity = null
+        var valueBoolean = null
+        for (var itemKey in theItem) {
+          const resource = this.mapping[itemKey]['resource'];
+          const parameter = this.mapping[itemKey]['parameter'];
+          const datatype = this.mapping[itemKey]['datatype'];
+          const unit = this.mapping[itemKey]['unit'];
+          if (resource == "Patient") {
+            if (parameter == "identifier") {
+              identifierValue = theItem[itemKey];
+              identifierCode = itemKey;
+            }
+            if (parameter == "gender") {
+              genderValue = theItem[itemKey];
+            }
+            if (parameter == "deceased") {
+              deceasedValueNumber = theItem[itemKey];
+              if (deceasedValue == 1) {
+                deceasedValueNumber = true
+              }
+              else if (deceasedValue == 0) {
+                deceasedValueNumber = false
+              }
+            }
+            const patientData = {
+              "resourceType": "Patient",
+              "identifier": [{
+                "use": "usual",
+                "type": {
+                  "coding": [{
+                    "system": this.codingSystem,
+                    "code": identifierCode
+                  }]
+                },
+                "value": identifierValue
+              }],
+              "gender": genderValue,
+              "deceasedBoolean": deceasedValue,
+            };
+            //console.log(patientData)
+            //await this.createFhirResource(patientData)
+            const client = FHIR.client("http://localhost:8080/fhir/");
+            const createFhirResource = await client.create(patientData);
+          }
+          if (resource == "Observation") {
+            if (datatype == "String") {
+              valueString = theItem[itemKey];
+            } else if (datatype == "Integer") {
+              valueQuantity = {
+                "value": theItem[itemKey],
+                "unit": unit,
+                "system": this.codingSystem,
+                "code": itemKey,
+              };
+            } else if (datatype == "Boolean") {
+              valueBoolean = {
+                "valueBoolean": theItem[itemKey],
+              };
+            }
+            const observationData = {
+              "resourceType": "Observation",
+              /*"subject": {
+                "reference": "Patient/" + identifierValue,
+                "display": identifierValue
+              },*/
+              "subject": {
+                "resource": "Patient",
+                "identifer": {
+                  "system": this.codingSystem,
+                  "value": identifierValue
+                }
+              },
+              "code": {
+                "coding": [{
+                  "system": this.codingSystem,
+                  "code": itemKey,
+                  "display": itemKey
+                }]
+              },
+              "valueQuantity": valueQuantity,
+              "valueString": valueString,
+              "valueBoolean": valueString,
+            };
+            //console.log(observationData)
+            //await this.createFhirResource(observationData, "Observation")
+          }
+        }
+      }
     }
   },
 };
 </script>
+
+
+
